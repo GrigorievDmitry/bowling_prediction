be4c6c7035f45a04604d7c271bee2a7d0aeba7ce
GrigorievDmitry <36789339+GrigorievDmitry@users.noreply.github.com>
2019-08-07 17:47:45 +0300

    Revert "Second predictor added to BallMotion"
    This reverts commit dbdf90ea4b398afacac2323744bf151f86d766fb.

--- BallMotion.cs
+++ BallMotion.cs
@@ -15,14 +15,12 @@ namespace VRABowling
         readonly int Port = 9292;
         bool connected = false;
 
-        private Predictor pred, second_pred;
+        private Predictor pred;
         private float delay_mean;
-        public Vector3 pos, second_pos;
+        public Vector3 pos;
 
         Vector3 offset = Vector3.Zero;
         Vector3 scale = Vector3.One;
-        Vector3 second_offset = Vector3.Zero;
-        Vector3 second_scale = Vector3.One;
 
         int detNum;
         float gridStep, ballRadius, delay, shift;
@@ -45,9 +43,7 @@ namespace VRABowling
         void StartPredictor()
         {
             pred = new Predictor(detNum, gridStep, ballRadius, delay, shift);
-            second_pred = new Predictor(detNum, gridStep, ballRadius, delay, shift);
             pred.Start();
-            second_pred.Start();
         }
 
         void Update()
@@ -55,10 +51,6 @@ namespace VRABowling
             pos = pred.Predict(delay_mean);
             pos = Vector3.Multiply(pos, scale);
             pos += offset;
-
-            second_pos = second_pred.Predict(delay_mean);
-            second_pos = Vector3.Multiply(second_pos, second_scale);
-            second_pos += second_offset;
         }
 
         public void SetInputConnection()
@@ -115,7 +107,7 @@ namespace VRABowling
         void Respond()
         {
             Update();
-            conn.Send(Encoding.Default.GetBytes(pos.ToString() + "\n" + second_pos.ToString() + "\n"));
+            conn.Send(Encoding.Default.GetBytes(pos.ToString() + "\n"));
         }
 
         void SetParameter(string input)
@@ -127,8 +119,6 @@ namespace VRABowling
                 float.Parse(adjValues[1].Value), float.Parse(adjValues[2].Value));
             if (words[0] == "offset") { offset = value; }
             if (words[0] == "scale") { scale = value; }
-            if (words[0] == "second_offset") { second_offset = value; }
-            if (words[0] == "second_scale") { second_scale = value; }
         }
 
         void Disconnect()
@@ -140,7 +130,6 @@ namespace VRABowling
         public void Stop()
         {
             pred.Disconnect();
-            second_pred.Disconnect();
             socket.Close();
         }
 
